c263c8ac9abe3505b22e206d9856a24a13f53d60
Shamil Irfan <shamilirfan28@gmai.com>
2025-11-04 12:27:45 +0600

    .

--- handlers/create_order.go
+++ handlers/create_order.go
@@ -9,7 +9,7 @@ import (
 func CreateOrder(db *sql.DB, order *models.Order) (err error) {
 	query := `
 		INSERT INTO orders (
-			customer_name, 
+			customer_id, 
 			road_number,
 			holding_number,
 			area, district, 
@@ -19,8 +19,15 @@ func CreateOrder(db *sql.DB, order *models.Order) (err error) {
 		VALUES ($1, $2, $3, $4, $5, $6,'Pending')
 		RETURNING id, created_at
 	`
+	/*
+		👉 QueryRow() এর কাজ হলো — একটা single row (একটা record) ডাটাবেস থেকে নিয়ে আসা।
+			একটি row রিটার্ন করে। যদি query তে একাধিক row ফেরত আসে, শুধু প্রথমটা নেয়।
+
+		👉 Scan() এর কাজ হলো — QueryRow() থেকে পাওয়া সেই একটি row-এর কলাম ভ্যালুগুলোকে
+			Go ভ্যারিয়েবলগুলোর মধ্যে কপি করে দেওয়া।
+	*/
 	err = db.QueryRow(query,
-		order.CustomerName,
+		order.CustomerID,
 		order.RoadNumber,
 		order.HoldingNumber,
 		order.Area,
@@ -47,13 +54,21 @@ func CreateOrder(db *sql.DB, order *models.Order) (err error) {
 		order.Items[i].BookName = name
 		order.Items[i].BookImage = image
 		order.Items[i].UnitPrice = price
-		order.Items[i].Subtotal = price * float64(order.Items[i].Quantity)
-		total += order.Items[i].Subtotal
+		order.Items[i].TotalPrice = price * float64(order.Items[i].Quantity)
+		total += order.Items[i].TotalPrice
 
 		query = `
-			INSERT INTO order_items (order_id, book_id, book_name, book_image, quantity, unit_price, subtotal)
+			INSERT INTO order_items (order_id, book_id, book_name, book_image, quantity, unit_price, total_price)
 			VALUES ($1, $2, $3, $4, $5,  $6,  $7)
 		`
+		/*
+			👉 db.Exec() কোনো row ফেরত দেয় না — শুধু ডাটাবেসে একটা রেকর্ড insert, update, delete করে।
+				db.Exec() দুইটা জিনিস ফেরত দেয়:
+				1.
+					. sql.Result → এতে থাকে: LastInsertId() — সর্বশেষ insert করা row-এর ID (যদি DB সমর্থন করে)
+					. RowsAffected() — কতগুলো row পরিবর্তন হয়েছে।
+				2.	.err → error হলে সেটা ধরে রাখে।
+		*/
 		_, err = db.Exec(
 			query,
 			order.ID,
@@ -62,20 +77,13 @@ func CreateOrder(db *sql.DB, order *models.Order) (err error) {
 			order.Items[i].BookImage,
 			order.Items[i].Quantity,
 			order.Items[i].UnitPrice,
-			order.Items[i].Subtotal,
+			order.Items[i].TotalPrice,
 		)
 		if err != nil {
 			return fmt.Errorf("failed to insert order item: %w", err)
 		}
 	}
 
-	query = `UPDATE orders SET total_price = $1 WHERE id = $2`
-	_, err = db.Exec(query, total, order.ID)
-	if err != nil {
-		return err
-	}
-
-	order.TotalPrice = total
 	order.Status = "Pending"
 	return nil
 }
--- models/models.go
+++ models/models.go
@@ -18,25 +18,24 @@ type Customer struct {
 
 type Order struct {
 	ID            int         `json:"id"`
-	CustomerName  string      `json:"customer_name"`  
+	CustomerID    int         `json:"customer_id"`
 	RoadNumber    string      `json:"road_number"`
 	HoldingNumber string      `json:"holding_number"`
 	Area          string      `json:"area"`
 	District      string      `json:"district"`
 	PhoneNumber   string      `json:"phone_number"`
-	TotalPrice    float64     `json:"total_price"`
 	Status        string      `json:"status"`
 	CreatedAt     time.Time   `json:"created_at"`
 	Items         []OrderItem `json:"items"`
 }
 
 type OrderItem struct {
-	ID        int     `json:"id"`
-	OrderID   int     `json:"order_id"`
-	BookID    int     `json:"book_id"`
-	BookName  string  `json:"book_name"`
-	BookImage string  `json:"book_image"`
-	Quantity  int     `json:"quantity"`
-	UnitPrice float64 `json:"unit_price"`
-	Subtotal  float64 `json:"subtotal"`
+	ID         int     `json:"id"`
+	OrderID    int     `json:"order_id"`
+	BookID     int     `json:"book_id"`
+	BookName   string  `json:"book_name"`
+	BookImage  string  `json:"book_image"`
+	Quantity   int     `json:"quantity"`
+	UnitPrice  float64 `json:"unit_price"`
+	TotalPrice float64 `json:"total_price"`
 }
